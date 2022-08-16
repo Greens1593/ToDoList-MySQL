25cde1ec5eda466b041921a864e898f94cf61d60
Mike Green <greens1593@gmail.com>
2022-08-16 20:30:13 +0300

    API routes was created and plug in index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const todoRoutes = require('./routes/todo.js')
+
 const PORT = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('api/todo', todoRoutes)
+
 app.use((req, res, next) => {
     res.sendFile('/index.html')
-})
+});
+
 
 app.listen(PORT)
+
+console.log('Server was start in port:', PORT)
---
+++ routes/todo.js
@@ -0,0 +1,28 @@
+const {Router} = require('express');
+const router = Router();
+
+// Получение списка задач
+
+router.get('/', (res, req) => {
+
+})
+
+// Создание новой задачи
+
+router.post('/', (res, req) => {
+
+})
+
+// Модификация задачи
+
+router.put('/:id', (req, res) => {
+
+})
+
+// Удаление задачи
+
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
